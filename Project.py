f93dd51d12b27064c519e7714ab21112fb34b826
lokol25 <lokol.25@mail.ru>
2024-07-16 21:21:23 +0500

    Add files via upload

---
+++ Project.py
@@ -0,0 +1,15 @@
+# 1Prodject
+print ( 9 ** 0.5 * 5)
+
+# 2Prodject
+print (9.99 > 9.98 and 1000 != 1000.1 )
+
+# 3Project
+print (1234 // 10 % 100 + 5678 // 10 % 100 )
+
+# 4Prodject
+a, b = 13.42, 42.13
+a1, b1 = int(a), int(b)
+a2, b2 = int((a - a1) * 100), int((b - b1) * 100)
+print(a1 == b2 or b1 == a2)
+
